82114900acdc1f7f5be4b020744d96dc48d02f36
olvalaskevich <olvol269@gmail.com>
2023-09-28 16:06:10 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Todolist/>
+            <Todolist/>
+            <Todolist/>
+        </div>
+    );
+}
+
+
+
+const Todolist = () => {
+    return (
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+};
+
+export default App;
+
