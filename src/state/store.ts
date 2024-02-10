af000ef0bad87432033ac8d58ddbfb8c488da9b4
olvalaskevich <olvol269@gmail.com>
2024-02-10 16:15:44 +0300

    add reducer for app

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,13 +1,15 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import {TasksActionType, todolistReducer} from "./todolists-reducer";
 import {tasksReducer, TasksReducerActionType} from "./tasks-reducer";
-import {thunk, ThunkAction, ThunkActionDispatch} from "redux-thunk";
+import {thunk, ThunkAction} from "redux-thunk";
+import {AppActionType, appReducer} from "./app-reducer";
 
-type AppActionCreatorsTypes= TasksReducerActionType | TasksActionType
+type AppActionCreatorsTypes= TasksReducerActionType | TasksActionType | AppActionType
 export type AppActionsTypes= ThunkAction<void, AppRootState, unknown, AppActionCreatorsTypes>
 export const rootReducer=combineReducers({
     todolists:todolistReducer,
-    tasks:tasksReducer
+    tasks:tasksReducer,
+    app: appReducer
 })
 
 export type AppRootState=ReturnType<typeof rootReducer>
