55bd4e0478fc3b055f6567ae5b93428b4401df89
olvalaskevich <olvol269@gmail.com>
2024-02-21 15:27:18 +0300

    add isInitialised AC

--- src/state/app-reducer.ts
+++ src/state/app-reducer.ts
@@ -3,19 +3,23 @@ export type statusType='idle'|'loading'|'success'
 export type appStateType={
     status:statusType
     error:string|null
+    isInitialized:boolean
 }
 const initialState:appStateType={
     status:'idle',
     error:null,
+    isInitialized:false
 }
 export type AppActionType=ReturnType<typeof appStatusAC> |
-    ReturnType<typeof appErrorAC>
+    ReturnType<typeof appErrorAC> | ReturnType<typeof setIsInitialisedAC>
 export const appReducer=(state:appStateType=initialState, action:AppActionType)=>{
     switch (action.type) {
         case 'SET-STATUS':
             return {...state, status:action.status}
         case 'SET-ERROR':
             return {...state, error:action.error}
+        case 'SET-IS-INITIALISED':
+            return {...state, isInitialized:action.isInitialized}
         default:
             return state
     }
@@ -26,4 +30,7 @@ export const appStatusAC=(status:statusType)=>{
 }
 export const appErrorAC=(error:string|null)=>{
     return ({type:'SET-ERROR', error: error} as const)
+}
+export const setIsInitialisedAC=(isInitialized:boolean)=>{
+    return ({type:'SET-IS-INITIALISED', isInitialized: isInitialized} as const)
 }
