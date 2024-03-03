2bf070edac03101371f0555258afb6734c458c6d
olvalaskevich <olvol269@gmail.com>
2024-03-03 13:22:15 +0300

    add auth and login reducers

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,15 +1,20 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import {TasksActionType, todolistReducer} from "./todolists-reducer";
 import {tasksReducer, TasksReducerActionType} from "./tasks-reducer";
-import {thunk, ThunkAction} from "redux-thunk";
+import {thunk, ThunkAction, ThunkDispatch} from "redux-thunk";
 import {AppActionType, appReducer} from "./app-reducer";
+import {AuthActionsType, authReducer} from "./auth-reducer";
+import {LoginActionsType, loginReducer} from "./login-reducer";
 
-type AppActionCreatorsTypes= TasksReducerActionType | TasksActionType | AppActionType
+type AppActionCreatorsTypes= TasksReducerActionType | TasksActionType | AppActionType | AuthActionsType | LoginActionsType
 export type AppActionsTypes= ThunkAction<void, AppRootState, unknown, AppActionCreatorsTypes>
+export type AppDispatchType=ThunkDispatch<AppRootState, unknown, AppActionCreatorsTypes>
 export const rootReducer=combineReducers({
     todolists:todolistReducer,
     tasks:tasksReducer,
-    app: appReducer
+    app: appReducer,
+    auth:authReducer,
+    login:loginReducer
 })
 
 export type AppRootState=ReturnType<typeof rootReducer>
