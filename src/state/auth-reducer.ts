0c54f4eaff202f2bb63086bb62b7e50607439b22
olvalaskevich <olvol269@gmail.com>
2024-03-15 20:46:29 +0300

    refactoring to redux toolkit

--- src/state/auth-reducer.ts
+++ src/state/auth-reducer.ts
@@ -1,60 +1,68 @@
 import {setIsInitialisedAC} from "./app-reducer";
 import {AppActionsTypes} from "./store";
 import {authAPI} from "../api/todolistsAPI";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {Dispatch} from "redux";
 
 export type DataAuthResponseType={
     id: number|null
     email: string
     login: string
 }
-export type AuthStateType={
-    data: DataAuthResponseType
-    resultCode: number|null
-    messages:string[]
-    isAuth:boolean
-}
+
 export type AuthActionsType=ReturnType<typeof setIsAuthAC> |
     ReturnType<typeof isAuthAC>
 
 let initialState={
-    data:{
-        id: null,
-        email: '',
-        login: ''
-    },
-    resultCode: null,
-    messages:[],
     isAuth:false
 }
-export const authReducer=(state:AuthStateType=initialState, action:AuthActionsType)=>{
-    switch (action.type) {
-        case 'SET-IS-AUTH':
-            return {...state, data:action.data, isAuth:action.isAuth}
-        case 'IS-AUTH':
-            return {...state, isAuth:action.isAuth}
-        default:
-            return state
+
+const slice=createSlice({
+    name:'auth',
+    initialState:initialState,
+    reducers:{
+        setIsAuthAC (state, action:PayloadAction<{isAuth:boolean}>){
+
+            state.isAuth=action.payload.isAuth
+        },
+        isAuthAC (state, action:PayloadAction<{isAuth:boolean}>){
+            state.isAuth=action.payload.isAuth
+        }
     }
-}
+})
+export const authReducer=slice.reducer
+export const setIsAuthAC=slice.actions.setIsAuthAC
+export const isAuthAC=slice.actions.isAuthAC
+// export const authReducer=(state:AuthStateType=initialState, action:AuthActionsType)=>{
+//     switch (action.type) {
+//         case 'SET-IS-AUTH':
+//             return {...state, data:action.data, isAuth:action.isAuth}
+//         case 'IS-AUTH':
+//             return {...state, isAuth:action.isAuth}
+//         default:
+//             return state
+//     }
+// }
 
-export const setIsAuthAC=(data:DataAuthResponseType, isAuth:boolean)=>{
-    return ({type:'SET-IS-AUTH',data:data, isAuth: isAuth} as const)
-}
-export const isAuthAC=(isAuth:boolean)=>{
-    return ({type:'IS-AUTH',isAuth: isAuth} as const)
-}
+// export const setIsAuthAC=(data:DataAuthResponseType, isAuth:boolean)=>{
+//     return ({type:'SET-IS-AUTH',data:data, isAuth: isAuth} as const)
+// }
+// export const isAuthAC=(isAuth:boolean)=>{
+//     return ({type:'IS-AUTH',isAuth: isAuth} as const)
+// }
 
 
-export const setIsAuthTC=():AppActionsTypes=>{
-    return (dispatch) => {
+export const setIsAuthTC=()=>{
+    return (dispatch:Dispatch) => {
         authAPI.setIsAuth()
             .then((res) => {
                 dispatch(setIsInitialisedAC(true))
                 if (res.data.resultCode === 0) {
-                    dispatch(setIsAuthAC(res.data.data, true))
+                    dispatch(setIsAuthAC({isAuth:true}))
                 } else {
-                    dispatch(setIsAuthAC(res.data.data, false))
+                    dispatch(setIsAuthAC({isAuth:true}))
                 }
+
             })
     }
 }
