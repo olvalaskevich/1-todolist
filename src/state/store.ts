847765c8d36b4d72ea07b6425055422557135e6d
olvalaskevich <olvol269@gmail.com>
2024-03-23 21:13:17 +0300

    delete unused login reducer

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,21 +1,19 @@
-import {applyMiddleware, combineReducers} from "redux";
+import {combineReducers} from "redux";
 import {TasksActionType, todolistReducer} from "./todolists-reducer";
 import {tasksReducer, TasksReducerActionType} from "./tasks-reducer";
-import {thunk, ThunkAction, ThunkDispatch} from "redux-thunk";
+import {ThunkAction, ThunkDispatch} from "redux-thunk";
 import {AppActionType, appReducer} from "./app-reducer";
 import {AuthActionsType, authReducer} from "./auth-reducer";
-import {LoginActionsType, loginReducer} from "./login-reducer";
 import {configureStore} from "@reduxjs/toolkit";
 
-type AppActionCreatorsTypes= TasksReducerActionType | TasksActionType | AppActionType | AuthActionsType | LoginActionsType
+type AppActionCreatorsTypes= TasksReducerActionType | TasksActionType | AppActionType | AuthActionsType
 export type AppActionsTypes= ThunkAction<void, AppRootState, unknown, AppActionCreatorsTypes>
 export type AppDispatchType=ThunkDispatch<AppRootState, unknown, AppActionCreatorsTypes>
 export const rootReducer=combineReducers({
     todolists:todolistReducer,
     tasks:tasksReducer,
     app: appReducer,
-    auth:authReducer,
-    login:loginReducer
+    auth:authReducer
 })
 
 export type AppRootState=ReturnType<typeof rootReducer>
