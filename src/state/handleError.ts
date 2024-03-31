97c2db3aa677668f56190c58012419fd27f972cb
olvalaskevich <olvol269@gmail.com>
2024-03-31 14:27:09 +0300

    add handleErrorOrigin

--- src/state/handleError.ts
+++ src/state/handleError.ts
@@ -1,13 +1,11 @@
-import {isAxiosError} from "axios";
 import {Dispatch} from "redux";
 import {appErrorAC} from "./app-reducer";
-import {ResponseType} from "../api/todolistsAPI";
 
 
 
 export const handleError=(e:any, dispatch:Dispatch)=>{
-    dispatch(appErrorAC(e.data.messages[0]))
+    dispatch(appErrorAC({error:e.data.messages[0]}))
 }
-export const handleErrorSetTasks=(e:string|null, dispatch:Dispatch)=>{
+export const handleErrorOrigin=(e:string|null, dispatch:Dispatch)=>{
     dispatch(appErrorAC({error:e}))
 }
