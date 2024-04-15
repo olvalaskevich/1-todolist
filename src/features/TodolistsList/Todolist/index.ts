f9c366b42f133d545af09131d8dac7db490389b8
olvalaskevich <olvol269@gmail.com>
2024-04-15 20:55:10 +0300

    add actions

--- src/features/TodolistsList/Todolist/index.ts
+++ src/features/TodolistsList/Todolist/index.ts
@@ -1,5 +1,21 @@
 import * as TodolistSelectors from './selectors'
+import {AsyncTasksActions} from './tasks-reducer'
+import {AsyncTodolistsActions} from './todolists-reducer'
+import {slice as sliceTd} from './todolists-reducer'
+import {slice as sliceTask} from './tasks-reducer'
+
+
+const TodolistsActions={
+    ...AsyncTodolistsActions,
+    ...sliceTd.actions
+}
+const TasksActions={
+    ...AsyncTasksActions,
+    ...sliceTask.actions
+}
 
 export {
-    TodolistSelectors
+    TodolistSelectors,
+    TasksActions,
+    TodolistsActions
 }
