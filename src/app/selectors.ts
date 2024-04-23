815a12081f23c9609f685cda2f1830878ef0bc5c
olvalaskevich <olvol269@gmail.com>
2024-04-23 16:30:43 +0300

    add selectors

--- src/app/selectors.ts
+++ src/app/selectors.ts
@@ -3,3 +3,5 @@ import {AppRootState} from "./store";
 
 export const statusSelector=(state:AppRootState)=>state.app.status
 export const initSelector=(state:AppRootState)=>state.app.isInitialized
+
+export const errorSelector=(state:AppRootState)=>state.app.error
--- src/features/TodolistsList/Todolist/selectors.ts
+++ src/features/TodolistsList/Todolist/selectors.ts
@@ -2,3 +2,4 @@ import {AppRootState} from "../../../app/store";
 
 
 export const tasksSelector=(state:AppRootState)=>state.tasks
+export const todolistsSelector=(state:AppRootState)=>state.todolists
