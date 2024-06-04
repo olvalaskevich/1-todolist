64bc43c1ae01adc91dc7ca61e3b29847e255ee7f
olvalaskevich <olvol269@gmail.com>
2024-06-04 19:45:20 +0300

    add tests for all cases of reducer

--- src/features/TodolistsList/Todolist/todolists-reducer.test.ts
+++ src/features/TodolistsList/Todolist/todolists-reducer.test.ts
@@ -1,98 +1,78 @@
 import {v1} from "uuid";
 import {
     ChangeFilterTdAC,
+    CreateTodolistsTC, DeleteTodolistsTC,
     SetTodolistsAC,
-    todolistReducer
+    todolistReducer, UpdateTodolistsTC
 } from "./todolists-reducer";
+import {TodoListTitleType} from "../../../app/types";
+import {logOutTC} from "../../Auth/auth-reducer";
 
-// test('todolist should be removed', ()=> {
-//         let todolistId1 = v1();
-//         let todolistId2 = v1();
-//
-//         let todolists: Array<TodoListTitleType> = [
-//             {
-//                 id: todolistId1, title: 'What to learn', filter: 'all', addedDate: 'string',
-//                 order: 0, entityStatus:'idle'
-//             },
-//             {
-//                 id: todolistId2, title: 'What to bye', filter: 'all', addedDate: 'string',
-//                 order: 0, entityStatus:'idle'
-//             }
-//         ]
-//
-//         const endState = todolistReducer(todolists, RemoveTdAC(todolistId1))
-//
-//         expect(endState.length).toBe(1)
-//     }
-//
-// )
+let todolistId1 = v1();
+let todolistId2 = v1();
+let todolists:Array<TodoListTitleType>=[
+    {id:todolistId1, title:'What to learn', filter:'all', addedDate: 'string',
+        order: 0, entityStatus:'idle'},
+    {id:todolistId2, title:'What to bye', filter:'all', addedDate: 'string',
+        order: 0, entityStatus:'idle'}
+];
+test('todolists should be set', ()=>{
 
-// test('todolist should be added', ()=>{
-//     let todolistId1=v1();
-//     let todolistId2=v1();
-//
-//     let todolists:Array<TodoListTitleType>=[
-//         {id:todolistId1, title:'What to learn', filter:'all', addedDate: 'string',
-//             order: 0, entityStatus:'idle'},
-//         {id:todolistId2, title:'What to bye', filter:'all', addedDate: 'string',
-//             order: 0, entityStatus:'idle'}
-//     ]
-//
-//     const endState=todolistReducer(todolists, AddTdAC({id:todolistId1, title:'What to learn', addedDate: 'string',
-//         order: 0}))
-//
-//     expect(endState.length).toBe(3)
-//
-//
-// })
+    let todolists:Array<TodoListTitleType>=[]
 
-// test('title of todolist should be changed', ()=>{
-//     let todolistId1=v1();
-//     let todolistId2=v1();
-//
-//     let todolists:Array<TodoListTitleType>=[
-//         {id:todolistId1, title:'What to learn', filter:'all', addedDate: 'string',
-//             order: 0, entityStatus:'idle'},
-//         {id:todolistId2, title:'What to bye', filter:'all', addedDate: 'string',
-//             order: 0, entityStatus:'idle'}
-//     ]
-//
-//     const endState=todolistReducer(todolists, ChangeTitleTdAC(todolistId1, "Hello"))
-//
-//     expect(endState.length).toBe(2)
-//
-// })
+    const endState=todolistReducer(todolists, SetTodolistsAC({td:[{
+        id: '1',
+        title: 'string',
+        addedDate: 'string',
+        order: 0}]}))
 
-// test('filter of todolist should be changed', ()=>{
-//     let todolistId1=v1();
-//     let todolistId2=v1();
-//
-//     let todolists:Array<TodoListTitleType>=[
-//         {id:todolistId1, title:'What to learn', filter:'all', addedDate: 'string',
-//             order: 0, entityStatus:'idle'},
-//         {id:todolistId2, title:'What to bye', filter:'all', addedDate: 'string',
-//             order: 0, entityStatus:'idle'}
-//     ]
-//
-//     const endState=todolistReducer(todolists, ChangeFilterTdAC(todolistId1, 'active'))
-//
-//     expect(endState.length).toBe(2)
-//
-// })
+        expect(endState.length).toBe(1)
 
-// test('todolists should be set', ()=>{
-//     let todolists:Array<TodoListTitleType>=[]
-//
-//     let action=SetTodolistsAC([{
-//         id: '1',
-//         title: 'string',
-//         addedDate: 'string',
-//         order: 0
-//     }])
-//
-//     const endState=todolistReducer(todolists, action)
-//
-//         expect(endState.length).toBe(1)
-//
-//
-// })
+})
+test('todolist should be added', ()=>{
+    let td={
+        id: '2',
+        title: 'Market',
+        addedDate: 'string',
+        order: 0}
+
+    const endState=todolistReducer(todolists, CreateTodolistsTC.fulfilled({td}, '', {
+        title:'Market'}))
+
+    expect(endState.length).toBe(3)
+    expect(endState[0].title).toBe('Market')
+
+})
+test('title of todolist should be changed', ()=>{
+
+    const endState=todolistReducer(todolists, UpdateTodolistsTC.fulfilled({
+        id:todolistId1,
+        value: 'What to read'}, '', {idTd:todolistId1, title:'What to read'}))
+
+    expect(endState[0].title).toBe('What to read')
+
+})
+test('filter of todolist should be changed', ()=>{
+
+    const endState=todolistReducer(todolists, ChangeFilterTdAC({id:todolistId1, value:'active'}))
+
+    expect(endState[0].filter).toBe('active')
+    expect(endState[1].filter).toBe('all')
+
+})
+test('todolist should be removed', ()=> {
+
+    const endState = todolistReducer(todolists, DeleteTodolistsTC.fulfilled({id:todolistId1},'',{idTd:todolistId1}))
+
+    expect(endState.length).toBe(1)
+    expect(endState[0].title).toBe('What to bye')
+
+    })
+test('todolists should be clear when we logout', ()=> {
+
+    const endState = todolistReducer(todolists, logOutTC.fulfilled(false,''))
+
+    expect(endState.length).toBe(0)
+
+})
+
